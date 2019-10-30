6bac3794e3739b4539fd875d8a02b3e87e05b695
tiansong_lu <tiansong_lu@intsig.net>
2019-10-30 18:19:39 +0800

    [WN]lts005

--- router/router.go
+++ router/router.go
@@ -20,7 +20,7 @@ func InitRoute(router *gin.RouterGroup) {
 	})
 	apiRouter.GET("/get-name", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"name": "lts004",
+			"name": "lts005",
 		})
 	})
 	apiRouter.GET("/exit-system", func(c *gin.Context) {
